604355989a43cb0652532f1796d2582946f77270
Xi Ruoyao <xry111@xry111.site>
2023-07-29 23:48:10 +0800

    Add kernel-config infrastructure
    The kernel-config.py script takes a toml file containing a set of
    kernel configuration key-value pairs.  Then it parses the Kconfig files
    in a kernel source tree and render the given configuration as a
    LFS-style <screen> in a separate XML file.  The XML file can be used in
    the book with xinclude.
    
    Some "features":
    
    1. The lines are limited to 80 columns.
       If the text of the configuration option is too long, it will be
       trimmed; if the symbolic name of the option cannot fit in this line,
       a separate line will be used for it.
    2. If a configuration option is given but it does not exist in Kconfig
       files, the script will abort immediately.  This helps catching
       removed options.
    3. The script also aborts immediately if a configuration option is
       illegal, for example setting an option to 'M' while it cannot be a
       module.
    4. The infrastructure is not wired into the main Makefile.  It's because
       not all editors have the latest kernel tree, and even if they do the
       locations of the kernel tree are still different.  To update the
       generated XML files, use
       "make -C kernel-config KERNEL_TREE=/sources/linux-x.y.z".
    
    Backword incompatible change:
    
    The script no longer outputs "CONFIG_" prefix for the symbolic name.  It
    really does not make too much sense to waste 7 characters here because
    it's a common prefix for all options!
    
    A limitation:
    
    The script does not really validate the configuration.  Generally
    validating the configuration requires to solve the 3-CNF-SAT problem,
    which is NP-complete.

---
+++ kernel-config/kernel-config.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: MIT
+# Copyright 2023 The LFS Editors
+
+# Stupid script to render "mconf"-style kernel configuration
+# Usage: kernel-config.py [path to kernel tree] [needed config].toml
+# The toml file should be like:
+#   for bool and tristate:
+#     EXT4="*"
+#     DRM="*M"
+#     EXPERT=" "
+#     DRM_I915="*M"
+#   for choice:
+#     HIGHMEM64G="X"
+
+choice_bit = 1 << 30
+ind0 = 0
+ind1 = 0
+menu_id = 1
+stack = []
+
+expand_var_mp = { 'SRCARCH': 'x86' }
+
+def expand_var(s):
+    for k in expand_var_mp:
+        s = s.replace('$(' + k + ')', expand_var_mp[k])
+    return s
+
+def pop_stack(cond):
+    global ind0, ind1, stack
+    assert(cond(stack[-1][0]))
+    s, i0, i1, _ = stack[-1]
+    stack = stack[:-1]
+    ind0 -= i0
+    ind1 -= i1
+
+def pop_stack_while(cond):
+    while len(stack) and cond(stack[-1][0]):
+        pop_stack(cond)
+
+def cur_menu():
+    global stack
+    return stack[-1][3] if len(stack) else 0
+
+def parse_config(buf):
+    global ind0, ind1, stack, menu_id
+    is_menu = buf[0].startswith('menu')
+    key = buf[0].split()[1].strip()
+
+    deps = ['menu']
+    title = None
+    klass = None
+    for line in buf[1:]:
+        line = line.strip()
+        if line.startswith('depends on '):
+            new_deps = line[len('depends on '):].split('&&')
+            deps += [x.strip() for x in new_deps]
+        else:
+            for prefix in ['tristate', 'bool', 'string']:
+                if line.startswith(prefix + ' '):
+                    title = line[len(prefix) + 1:]
+                    klass = prefix
+                elif line.startswith('def_' + prefix + ' '):
+                    klass = prefix
+                elif line.startswith('prompt '):
+                    title = line[len('prompt '):]
+
+    pop_stack_while(lambda x: x not in deps)
+
+    if key not in known_config:
+        return []
+    val = known_config[key]
+
+    assert(title and klass)
+    title = title.strip('"')
+
+    if klass == 'string':
+        val = '(' + val + ')'
+    else:
+        assert((val == 'X') == bool(cur_menu() & choice_bit))
+        if (val == 'X'):
+            val = '(X)'
+        else:
+            for c in val:
+                if c not in 'M* ' or (c == 'M' and klass != 'tristate'):
+                    raise Exception('unknown setting %s for %s' % (c, key))
+            val = '/'.join(val)
+            if klass == 'tristate':
+                val = '<' + val + '>'
+            elif klass == 'bool':
+                val = '[' + val + ']'
+            else:
+                raise Exception('should not reach here')
+
+    arrow = ' --->' if is_menu else ''
+    r = [(ind0, val, ind1, title, arrow, key, cur_menu())]
+    menu_id += is_menu
+    stack_ent = (key, 2, 0, menu_id) if is_menu else (key, 0, 2, cur_menu())
+    ind0 += stack_ent[1]
+    ind1 += stack_ent[2]
+    stack += [stack_ent]
+    return r
+
+def parse_choice(buf):
+    global ind0, ind1, stack, menu_id
+    assert(buf[0] == 'choice\n')
+    title = ''
+    for line in buf:
+        line = line.strip()
+        if line.startswith('prompt '):
+            title = line[len('prompt '):].strip().strip('"')
+    r = [(ind0, "", ind1, title, ' --->', '', cur_menu())]
+    menu_id += 1
+    stack += [('menu', 2, 0, menu_id | choice_bit)]
+    ind0 += 2
+    return r
+
+def load_kconfig(file):
+    global ind0, ind1, stack, path, menu_id
+    r = []
+    config_buf = []
+    with open(path + file) as f:
+        for line in f:
+            if len(config_buf):
+                if not line.startswith('\t'):
+                    if config_buf[0] == 'choice\n':
+                        r += parse_choice(config_buf)
+                    else:
+                        r += parse_config(config_buf)
+                    config_buf = []
+                else:
+                    config_buf += [line]
+                    continue
+            if line.startswith('source'):
+                sub = expand_var(line.split()[1].strip('"'))
+                r += load_kconfig(sub)
+            elif line.startswith('config') or line.startswith('menuconfig'):
+                config_buf = [line]
+            elif line.startswith('choice'):
+                config_buf = [line]
+            elif line.startswith("menu"):
+                title = expand_var(line[4:].strip().strip('"'))
+                r += [(ind0, "", ind1, title, ' --->', '', cur_menu())]
+                menu_id += 1
+                stack += [('menu', 2, 0, menu_id)]
+                ind0 += 2
+            elif line.startswith('endmenu') or line.startswith('endchoice'):
+                pop_stack_while(lambda x: x != 'menu')
+                pop_stack(lambda x: x == 'menu')
+                if r[-1][1] == "":
+                    # prune empty menu
+                    r = r[:-1]
+    return r
+
+known_config = {}
+
+from sys import argv
+import tomllib
+
+path = argv[1]
+if path[-1] != '/':
+    path += '/'
+with open(argv[2], 'rb') as f:
+    known_config = tomllib.load(f)
+
+r = load_kconfig("Kconfig")
+
+# Now we are going to pretty-print r
+
+## Calculate the maximum value length for each menu
+max_val_len = {}
+for _, val, _, _, _, _, menu in r:
+    x = max_val_len[menu] if menu in max_val_len else 0
+    max_val_len[menu] = max(x, len(val))
+
+## Output
+
+max_line = 80
+buf = []
+
+done = [x[5] for x in r]
+for i in known_config:
+    assert(i in done)
+
+for i0, val, i1, title, arrow, key, menu in r:
+    if val:
+        val += (max_val_len[menu] - len(val)) * ' '
+    line = i0 * ' ' + val + (i1 + bool(val)) * ' '
+
+    rem = max_line - len(line) - len(arrow)
+
+    if len(title) > rem:
+        title = title[:rem - 3] + '...'
+
+    line += title + arrow
+    rem = max_line - len(line)
+
+    if key:
+        key = ' [' + key + ']'
+
+    if len(key) <= rem:
+        line += (rem - len(key)) * ' ' + key
+    else:
+        key = '... ' + key
+        line += '\n' + ' ' * (max_line - len(key)) + key
+    buf += [line.replace('<', '&lt;').replace('>', '&gt;').rstrip()]
+
+import kernel_version
+kver = kernel_version.kernel_version(path)
+
+print('''<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE note PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN"
+  "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd">''')
+print('<!-- Automatically generated by kernel-config.py for Linux', kver)
+print('     DO NOT EDIT! -->')
+print('<screen><literal>' + '\n'.join(buf) + '</literal></screen>')
